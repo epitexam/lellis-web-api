f7b79d42f081842c2fed316deddaeeb0502b170b
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-10-24 07:57:01 +0200

    Creating CreateUserComposer

---
+++ src/infrastructure/user/services/CreateUserComposer.ts
@@ -0,0 +1,16 @@
+import { IPasswordHasher } from "../../../application/providers/IPasswordHasher";
+import { IUsersRepository } from "../../../application/repositories/IUsersRepository";
+import { CreateUserUseCase } from "../../../application/useCases/user/CreateUserUseCase";
+import { ICreateUserUseCase } from "../../../application/useCases/user/ICreateUserUseCase";
+import { CreateUserController } from "../../../presentation/http/user/CreateUserController";
+import { PasswordHasher } from "../providers/HashProviser";
+import { PrismaUserRepository } from "../repositories/PrismaUserRepository";
+
+export function CreateUserComposer() {
+    const repository: IUsersRepository = new PrismaUserRepository();
+    const passwordHasher: IPasswordHasher = new PasswordHasher();
+    const useCase: ICreateUserUseCase = new CreateUserUseCase(repository, passwordHasher);
+    const controller = new CreateUserController(useCase);
+
+    return controller
+}
