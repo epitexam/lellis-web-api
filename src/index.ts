ea98ce327d5ea5135fdb4debbfa50a159de0cc21
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-10-26 00:24:49 +0200

    Adding jwt configuration to elysia app

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import { Elysia } from "elysia";
 import { registerUserRoutes } from "./presentation/elysia/user";
+import { jwt } from "@elysiajs/jwt";
 
 const PORT = Number(process.env.PORT) || 3000;
 
 export const app = new Elysia()
+
+app.use(
+    jwt({
+        name: "jwt",
+        secret: process.env.JWT_SECRET || "super_secret_key",
+    })
+);
 
 app.use(registerUserRoutes)
 app.listen(PORT);
