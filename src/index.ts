10905d4b76bd34f8c4072fa700cb1a08b0cabd34
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-10-25 22:56:41 +0200

    Removing useless code

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,11 @@
 import { Elysia } from "elysia";
-import { authMiddleware } from "./presentation/http/middleware/authMiddleware";
+import { registerUserRoutes } from "./presentation/elysia/user";
 
 const PORT = Number(process.env.PORT) || 3000;
 
 export const app = new Elysia()
-    .group('/user', (app) =>
-        app
-            .post('/sign-in', () => 'Sign in')
-            .post('/sign-up', () => 'Sign up')
-            .post('/profile', () => 'Profile')
-            .get("/profile", () => {},{
-                beforeHandle: [authMiddleware]
-            })
-    )
 
-
-
+app.use(registerUserRoutes)
 app.listen(PORT);
 
 console.log(`Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
