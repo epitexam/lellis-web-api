efcec3cc6d2db02052d069872afd011cf1af84c0
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-11-07 00:40:00 +0100

    Updating imports

--- src/infrastructure/user/services/CreateUserComposer.ts
+++ src/infrastructure/user/services/CreateUserComposer.ts
@@ -2,7 +2,7 @@ import { IPasswordHasher } from "../../../application/providers/IPasswordHasher"
 import { IUsersRepository } from "../../../application/repositories/IUsersRepository";
 import { CreateUserUseCase } from "../../../application/useCases/user/CreateUser/CreateUserUseCase";
 import { ICreateUserUseCase } from "../../../application/useCases/user/CreateUser/ICreateUserUseCase";
-import { CreateUserController } from "../../../presentation/http/user/CreateUserController";
+import { CreateUserController } from "../../../presentation/http/controller/user/CreateUserController";
 import { PasswordHasher } from "../providers/HashProviser";
 import { PrismaUserRepository } from "../repositories/PrismaUserRepository";
 
--- src/infrastructure/user/services/UpdateUserComposer.ts
+++ src/infrastructure/user/services/UpdateUserComposer.ts
@@ -2,7 +2,7 @@ import { IPasswordHasher } from "../../../application/providers/IPasswordHasher"
 import { IUsersRepository } from "../../../application/repositories/IUsersRepository";
 import { IUpdateUserUseCase } from "../../../application/useCases/user/updateUser/IUpdateUserUseCase";
 import { UpdateUserUseCase } from "../../../application/useCases/user/updateUser/UpdateUserUseCase";
-import { UpdateUserController } from "../../../presentation/http/user/UpdateUserController";
+import { UpdateUserController } from "../../../presentation/http/controller/user/UpdateUserController";
 import { PasswordHasher } from "../providers/HashProviser";
 import { PrismaUserRepository } from "../repositories/PrismaUserRepository";
 
