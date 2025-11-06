53b5954c53d5e626ff785bef555df44e218d3ace
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-11-07 00:41:20 +0100

    Finishing the implementation of various composers

---
+++ src/infrastructure/user/services/SearchUserComposer.ts
@@ -0,0 +1,13 @@
+import { IUsersRepository } from "../../../application/repositories/IUsersRepository";
+import { ISearchUsersUseCase } from "../../../application/useCases/user/SearchUser/ISearchUserUseCase";
+import { SearchUsersUseCase } from "../../../application/useCases/user/SearchUser/SearchUserUseCase";
+import { SearchUserController } from "../../../presentation/http/controller/user/SearchUserController";
+import { PrismaUserRepository } from "../repositories/PrismaUserRepository";
+
+export function SearchUserComposer() {
+    const repository: IUsersRepository = new PrismaUserRepository();
+    const useCase: ISearchUsersUseCase = new SearchUsersUseCase(repository);
+    const controller = new SearchUserController(useCase)
+
+    return controller
+}
