d4226b456bb870bb63576a7197e6a8d695c69739
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-11-06 09:58:46 +0100

    Implementing update user composer file

--- src/infrastructure/user/services/UpdateUserComposer.ts
+++ src/infrastructure/user/services/UpdateUserComposer.ts
@@ -1,9 +1,16 @@
 import { IPasswordHasher } from "../../../application/providers/IPasswordHasher";
 import { IUsersRepository } from "../../../application/repositories/IUsersRepository";
+import { IUpdateUserUseCase } from "../../../application/useCases/user/updateUser/IUpdateUserUseCase";
+import { UpdateUserUseCase } from "../../../application/useCases/user/updateUser/UpdateUserUseCase";
+import { UpdateUserController } from "../../../presentation/http/user/UpdateUserController";
 import { PasswordHasher } from "../providers/HashProviser";
 import { PrismaUserRepository } from "../repositories/PrismaUserRepository";
 
 export function UpdateUserComposer() {
     const repository: IUsersRepository = new PrismaUserRepository();
     const passwordHasher: IPasswordHasher = new PasswordHasher();
+    const useCase: IUpdateUserUseCase = new UpdateUserUseCase(repository, passwordHasher);
+    const controller = new UpdateUserController(useCase)
+
+    return controller
 }
