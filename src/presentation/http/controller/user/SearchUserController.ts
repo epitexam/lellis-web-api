7c3a4fb604b803ed15b4e8c152668c4b4237cac2
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-11-07 00:39:30 +0100

    Implementing search user controller

---
+++ src/presentation/http/controller/user/SearchUserController.ts
@@ -0,0 +1,18 @@
+import { ISearchUsersUseCase } from "../../../../application/useCases/user/SearchUser/ISearchUserUseCase";
+import { ISearchUsersInputDTO } from "../../../../domain/user/dtos/ISearchUsersInputDTO";
+import { IUserOutputRequestDTO } from "../../../../domain/user/dtos/IUserOutputRequestDTO";
+import { IController } from "../../interface/IController";
+
+export class SearchUserController implements IController<ISearchUsersInputDTO, IUserOutputRequestDTO[]> {
+    constructor(private readonly searchUserUseCase: ISearchUsersUseCase) { }
+
+    async handle(request: ISearchUsersInputDTO): Promise<IUserOutputRequestDTO[]> {
+        const result = await this.searchUserUseCase.execute(request)
+
+        if (!result.success || !result.data) {
+            throw new Error(result.error ?? "Failed to fetch user info.");
+        }
+
+        return result.data
+    }
+}
