0a0f3b97b039ddf70edbdd556c500ca415a125a2
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-11-06 09:58:28 +0100

    Implementing  update user controller file

---
+++ src/presentation/http/user/UpdateUserController.ts
@@ -0,0 +1,22 @@
+import { IUpdateUserUseCase } from "../../../application/useCases/user/updateUser/IUpdateUserUseCase";
+import { IUpdateUserRequestDTO } from "../../../domain/user/dtos/IUpdateUserRequestDTO";
+import { IUserOutputRequestDTO } from "../../../domain/user/dtos/IUserOutputRequestDTO";
+import { IController } from "../interface/IController";
+
+export class UpdateUserController implements IController<IUpdateUserRequestDTO, Partial<IUserOutputRequestDTO>> {
+    constructor(private readonly updateUserUseCase: IUpdateUserUseCase) { }
+
+    async handle(request: IUpdateUserRequestDTO): Promise<Partial<IUserOutputRequestDTO>> {
+        const { user_id, ...updateData } = request;
+
+        const result = await this.updateUserUseCase.execute(user_id, updateData)
+
+        if (!result.success || !result.data) {
+            throw new Error(result.error ?? "Failed to update user info.");
+        }
+
+        return {
+            uuid: result.data.uuid,
+        };
+    }
+}
