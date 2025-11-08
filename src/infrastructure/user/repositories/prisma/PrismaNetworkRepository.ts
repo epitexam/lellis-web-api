befe21a8d13abc6e94039daaa8c94742ab61cd95
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-11-08 14:07:03 +0100

    Creating PrismaNetworkRepositor (not implemented yet)

---
+++ src/infrastructure/user/repositories/prisma/PrismaNetworkRepository.ts
@@ -0,0 +1,34 @@
+import { INetworkRepository } from "../../../../application/repositories/INetworkRepository";
+import { ICreateNetworkDTO } from "../../../../domain/network/dtos/ICreateNetworkDTO";
+import { INetworkOutputRequestDTO } from "../../../../domain/network/dtos/INetworkOutputRequestDTO";
+import { IUpdateUserRequestDTO } from "../../../../domain/user/dtos/IUpdateUserRequestDTO";
+
+export class PrismaNetworkRepository implements INetworkRepository {
+    create(data: ICreateNetworkDTO): Promise<INetworkOutputRequestDTO> {
+        throw new Error("Method not implemented yet")
+    }
+
+    findAllNetworks(): Promise<INetworkOutputRequestDTO[]> {
+        throw new Error("Method not implemented yet")
+    }
+
+    findNetworkById(id: string): Promise<INetworkOutputRequestDTO | null> {
+        throw new Error("Method not implemented yet")
+    }
+
+    findNetworkByName(name: string): Promise<INetworkOutputRequestDTO[] | null> {
+        throw new Error("Method not implemented yet")
+    }
+
+    updateNetwork(id: string, data: IUpdateUserRequestDTO): Promise<INetworkOutputRequestDTO> {
+        throw new Error("Method not implemented yet")
+    }
+
+    deleteNetwork(id: string): Promise<Partial<INetworkOutputRequestDTO>> {
+        throw new Error("Method not implemented yet")
+    }
+
+    addMemberToNetwork(userId: string, networkId: string): Promise<Partial<INetworkOutputRequestDTO>> {
+        throw new Error("Method not implemented yet")
+    }
+}
